5c0c0f3496793c3a4eb2ff46ae4137878cf80e30
Abdur Rahman Bilal <aramb@aramservices.com>
2025-05-27 20:00:39 -0400

    Implement game logic, custom cursor, and sound controls
    Adds core gameplay mechanics to the Worm Wrangler game, including worm appearance, scoring, miss tracking, and a game timer.
    
    Introduces a custom cursor that changes appearance on mouse down and up events, enhancing user interaction. Styles for the custom cursor are added to the game page CSS.
    
    Implements sound control features on the intro page, allowing users to toggle sound and adjust volume. These settings are stored in session storage for use on the game page.
    
    Updates HTML structure on both game and intro pages to integrate new JavaScript functionalities and UI elements like score displays and buttons. CSS for the intro page is adjusted to hide volume controls initially.

--- javascript/gamePageScript.js
+++ javascript/gamePageScript.js
@@ -1,1 +1,179 @@
- 
+let openHand = sessionStorage.getItem("openHand");
+let closeHand = sessionStorage.getItem("closeHand");
+cursor.style.backgroundImage = `url("${openHand}")`;
+document.addEventListener("mousedown", function () {
+cursor.style.backgroundImage = `url("${closeHand}")`;
+cursor.style.transform = "rotate(-45deg)";
+});
+
+document.addEventListener("mouseup", function () {
+cursor.style.backgroundImage = `url("${openHand}")`;
+cursor.style.transform = "none";
+});
+
+
+ const homeButton = document.getElementById("homeButton");
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", function (eventObject) {
+cursor.style.left = eventObject.pageX - 20 + "px";
+cursor.style.top = eventObject.pageY - 20 + "px";
+});
+
+
+let sound = sessionStorage.getItem("sound");
+let volumeString = sessionStorage.getItem("volume");
+const startButton = document.getElementById("startButton");
+const holes = document.querySelectorAll(".hole");
+const missesDisplay = document.getElementById("misses");
+let misses = 0;
+const maxMisses = 8;
+const scoreDisplay = document.getElementById("score");
+const tray= document.querySelector(".tray");
+const wormCountDisplay = document.getElementById("wormCount");
+let score = 0;
+let count = 0;
+startButton.addEventListener("click", startGame);
+const gameBoard= document.querySelector(".gameBoard");
+gameBoard.addEventListener("click",function(eventObject){
+});
+gameBoard.addEventListener("click", function (eventObject) {
+if (gameOn && eventObject.target.className === "gameBoard") {
+missWorm();
+}
+});
+
+homeButton.addEventListener("click", function () {
+window.location.href = "index.html";
+});
+
+const timerElement = document.getElementById("timeLeft");
+let timeLeft = 20;
+const minCount = 6;
+let timerId1;
+let gameOn = false;
+function popUpWorm() {
+let wormDisplayTime = randomTime(500, 1000);
+const hole = randomHole(holes);
+const worm = hole.querySelector(".worm");
+worm.style.display = "block";
+worm.addEventListener("click", collectWorms);
+setTimeout(() => {
+worm.style.display = "none";
+worm.removeEventListener("click", collectWorms);
+if (gameOn && misses < maxMisses) {
+popUpWorm();
+}
+}, wormDisplayTime);
+}
+
+
+function randomTime(minimum, maximum) {
+return Math.floor(Math.random() * (maximum-minimum)) + minimum;
+}
+
+function randomHole(holes) {
+const index = Math.floor(Math.random() * holes.length);
+const hole = holes[index];
+return hole;
+}
+
+
+
+
+let timerId;
+function startGame() {
+    score = 0;
+misses = 0;
+timeLeft = 20;
+count = 0;
+scoreDisplay.textContent = score;
+missesDisplay.textContent = misses;
+wormCountDisplay.textContent = count;
+timerElement.textContent = "20";
+resetTray();
+gameOn = true;
+startButton.disabled = true;
+homeButton.disabled = true;
+timerId1 = setInterval(updateTimer, 1000);
+for(i=0; i<holes.length; i++){
+holes[i].addEventListener("click", missWorm);
+}
+popUpWorm();
+timerId = setTimeout(() => {
+for(i=0; i<holes.length; i++){
+holes[i].removeEventListener("click", missWorm);
+}
+gameOn = false;
+startButton.disabled = false;
+homeButton.disabled = false;
+if (count >= minCount) {
+alert("Congratulations, you won! Your score is " + score +".");
+} else {
+alert("You have slipped a bit! Your score is " + score +".");
+}
+}, 20000);
+}
+function missWorm() {
+if (misses < maxMisses) {
+misses++;
+missesDisplay.textContent = misses;
+if (misses >= maxMisses) {
+startButton.disabled = false;
+homeButton.disabled = false;
+gameOn = false;
+alert("Too many misses! You have slipped a bit! Your score is " +
+score +".");
+clearTimeout(timerId);
+clearInterval(timerId1);
+}
+}
+}
+
+
+
+
+
+
+
+
+function missWorm() {
+if (misses < maxMisses) {
+misses++;
+missesDisplay.textContent = misses;
+if (misses >= maxMisses) {
+alert("Too many misses! You have slipped a bit! Your score is " +
+score +".");
+clearTimeout(timerId);
+clearInterval(timerId1);
+}
+}
+}
+
+function resetTray() {
+while (tray.firstChild) {
+tray.removeChild(tray.firstChild);
+}
+}
+
+
+function updateTimer() {
+timeLeft--;
+timerElement.textContent = timeLeft;
+if (timeLeft === 0) {
+clearInterval(timerId1);
+}
+}
+
+function collectWorms(eventObject) {
+eventObject.stopPropagation();
+const wormTarget = eventObject.target;
+wormTarget.style.display = "none";
+score += 5;
+count++;
+scoreDisplay.textContent = score;
+wormCountDisplay.textContent = count;
+const trayImg = document.createElement("img");
+trayImg.src = "images/wormtray.png";
+trayImg.alt = "Worm";
+tray.appendChild(trayImg);
+}
--- javascript/introPage.js
+++ javascript/introPage.js
@@ -1,1 +1,81 @@
- 
+const soundCheckBox = document.getElementById("soundCheckBox");
+const volumeSlider = document.getElementById("volumeSlider");
+const adjustVolume = document.getElementById("adjustVolume");
+
+soundCheckBox.addEventListener("change", soundCheck);
+volumeSlider.addEventListener("input", volumeCheck);
+
+let sound = "off";
+let volumeValue = "0";
+sessionStorage.setItem("sound", sound);
+sessionStorage.setItem("volume", volumeValue);
+
+function soundCheck() {
+if (soundCheckBox.checked) {
+volumeSlider.style.display = "block";
+adjustVolume.style.display = "block";
+sound = "on";
+sessionStorage.setItem("sound", sound);
+volumeValue = "5";
+sessionStorage.setItem("volume", volumeValue);
+} else {
+volumeSlider.style.display = "none";
+adjustVolume.style.display = "none";
+sound = "off";
+sessionStorage.setItem("sound", sound);
+volumeValue = "0";
+sessionStorage.setItem("volume", volumeValue);
+}
+}
+
+
+function volumeCheck(eventObject){
+volumeValue = eventObject.target.value;
+sessionStorage.setItem("volume", volumeValue);
+}
+
+
+
+const goToGameButton= document.getElementById("goToGameButton");
+goToGameButton.addEventListener("click", goToGame);
+
+function goToGame(){
+window.location.href = "gamePage.html";
+}
+
+const glove1 = document.getElementById("glove1");
+const glove2 = document.getElementById("glove2");
+const glove3 = document.getElementById("glove3");
+
+glove1.addEventListener("click", gloveSelect);
+glove2.addEventListener("click", gloveSelect);
+glove3.addEventListener("click", gloveSelect);
+
+sessionStorage.setItem("openHand", "images/openHand1.png");
+sessionStorage.setItem("closeHand", "images/closeHand1.png");
+
+function gloveSelect(eventObject){
+const glove=eventObject.target.id;
+if(glove === "glove1"){
+glove1.style.border = "2px solid black";
+glove2.style.border = "0px";
+glove3.style.border = "0px";
+sessionStorage.setItem("openHand", "images/openHand1.png");
+sessionStorage.setItem("closeHand", "images/closeHand1.png");
+
+}
+else if(glove === "glove2"){
+glove2.style.border = "2px solid black";
+glove1.style.border = "0px";
+glove3.style.border = "0px";
+sessionStorage.setItem("openHand", "images/openHand2.png");
+sessionStorage.setItem("closeHand", "images/closeHand2.png");
+}
+else{
+glove3.style.border = "2px solid black";
+glove1.style.border = "0px";
+glove2.style.border = "0px";
+sessionStorage.setItem("openHand", "images/openHand3.png");
+sessionStorage.setItem("closeHand", "images/closeHand3.png");
+}
+}
